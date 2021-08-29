963d7891c9525de104b749ccee143d6dc2e39b7d
Tandy Nguyen <nguyetan001@gmail.com>
2021-08-29 07:51:59 -0400

    Generate report object on main app

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const { oldPool, newPool } = require('./database');
+const { getOneTableName, getAllTableRows, getAllTableColumns } = require('./database/queries');
+const { createReportObject } = require('./utils/createReport');
+const { differentColumns } = require('./utils/compareObjects');
+
+const { convertArrayToObjectWithKeys } = require('./utils/modifyObjects');
+
+const generateReport = async () => {
+  const oldDBTableName = await getOneTableName(oldPool);
+  const newDBTableName = await getOneTableName(newPool);
+
+  const oldTableColumns = await getAllTableColumns(oldPool, oldDBTableName.table_name);
+  const newTableColumns = await getAllTableColumns(newPool, newDBTableName.table_name);
+
+  const differentTableColumns = differentColumns(oldTableColumns, newTableColumns);
+
+  const oldDBRows = await getAllTableRows(oldPool, oldDBTableName.table_name);
+  const newDBRows = await getAllTableRows(newPool, newDBTableName.table_name);
+
+  await oldPool.end();
+  await newPool.end();
+
+  const modifiedOldDBRows = convertArrayToObjectWithKeys(oldDBRows, 'id');
+  const modifiedNewDBRows = convertArrayToObjectWithKeys(newDBRows, 'id');
+
+  const reportObject = createReportObject(modifiedOldDBRows, modifiedNewDBRows, differentTableColumns);
+}
+
+generateReport();
