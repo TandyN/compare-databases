bc6a99f4fcdf8265877322051d2210ea705316f8
Tandy Nguyen <nguyetan001@gmail.com>
2021-08-29 05:43:16 -0400

    Add createReportObject function & tests

--- utils/createReport.js
+++ utils/createReport.js
@@ -1,3 +1,5 @@
+const { ifObjectsContainSameContent } = require('./compareObjects');
+
 const addToCorrupt = (objReport, objCorruptValues, objActualValues, primaryKey) => {
   objReport.corrupt[primaryKey] = {
     corruptValues: objCorruptValues,
@@ -9,7 +11,40 @@ const addToMissing = (objReport, objMain, primaryKey) => {
   objReport.missing[primaryKey] = objMain[primaryKey];
 };
 
+const createReportObject = (oldDBObject, newDBObject) => {
+  const reportObject = {
+    corrupt: {},
+    missing: {},
+    new: {},
+  }
+
+  for (let primaryKey in oldDBObject) {
+    if (newDBObject[primaryKey]) {
+      if (!ifObjectsContainSameContent(oldDBObject[primaryKey], newDBObject[primaryKey])) {
+        const corruptValues = {};
+        const actualValues = {};
+
+        for (let field in oldDBObject[primaryKey]) {
+          if (oldDBObject[primaryKey][field] !== newDBObject[primaryKey][field]) {
+            corruptValues[field] = newDBObject[primaryKey][field];
+            actualValues[field] = oldDBObject[primaryKey][field];
+          }
+        }
+        addToCorrupt(reportObject, corruptValues, actualValues, primaryKey);
+      }
+      delete newDBObject[primaryKey];
+    } else {
+      addToMissing(reportObject, oldDBObject, primaryKey)
+    }
+  }
+
+  reportObject.new = { ...newDBObject };
+
+  return reportObject;
+};
+
 module.exports = {
   addToCorrupt,
   addToMissing,
+  createReportObject,
 };
--- utils/createReport.test.js
+++ utils/createReport.test.js
@@ -1,6 +1,8 @@
+const { expect } = require('@jest/globals');
 const {
   addToCorrupt,
-  addToMissing
+  addToMissing,
+  createReportObject,
 } = require('./createReport');
 
 describe('Report Creating Functions', () => {
@@ -85,6 +87,152 @@ describe('Report Creating Functions', () => {
       expect(reportObject.missing['2']).toMatchObject({ name: 'Sam' });
       expect(Object.keys(reportObject.missing).length).toBe(2);
 
+    });
+  });
+
+  describe('createReportObject', () => {
+    it(`should put missing keys that the new DB does not have into the 'missing' key in the report object`, () => {
+      const testOldDB = {
+        '1': { name: '1' },
+        '2': { name: '2' },
+        '3': { name: '3' },
+        '4': { name: '4' },
+      };
+      const testNewDB = {
+        '3': { name: '3' },
+        '4': { name: '4' },
+      };
+
+      const testReport = createReportObject(testOldDB, testNewDB);
+
+      const expectedReportObject = {
+        corrupt: {},
+        missing: {
+          '1': { name: '1' },
+          '2': { name: '2' },
+        },
+        new: {},
+      }
+      expect(testReport).toEqual(expectedReportObject);
+    });
+
+    it(`should put keys that are found in the new DB and not in the old into the new key of the report object`, () => {
+      const testOldDB = {
+        '1': { name: '1' },
+        '2': { name: '2' },
+        '3': { name: '3' },
+      };
+      const testNewDB = {
+        '1': { name: '1' },
+        '2': { name: '2' },
+        '3': { name: '3' },
+        '4': { name: '4' },
+      };
+
+      const testReport = createReportObject(testOldDB, testNewDB);
+
+      const expectedReportObject = {
+        corrupt: {},
+        missing: {},
+        new: {
+          '4': { name: '4' },
+        },
+      }
+      expect(testReport).toEqual(expectedReportObject);
+    });
+
+    it(`should put corrupt values into the corrupt key in the report object`, () => {
+      const testOldDB = {
+        '1': { name: '1', email: '1' },
+        '2': { name: '2', email: '2' },
+        '3': { name: '3', email: '3' },
+      };
+      const testNewDB = {
+        '1': { name: '1', email: '2' },
+        '2': { name: '1', email: '2' },
+        '3': { name: '4', email: '4' },
+      };
+
+      const testReport = createReportObject(testOldDB, testNewDB);
+
+      const expectedReportObject = {
+        corrupt: {
+          '1': { corruptValues: { email: '2' }, actualValues: { email: '1' } },
+          '2': { corruptValues: { name: '1' }, actualValues: { name: '2' } },
+          '3': { corruptValues: { name: '4', email: '4' }, actualValues: { name: '3', email: '3' } },
+        },
+        missing: {},
+        new: {},
+      }
+      expect(testReport).toEqual(expectedReportObject);
+    });
+
+    it(`should be possible to have all 3 keys not empty`, () => {
+      const testOldDB = {
+        '1': { name: '1', email: '1' },
+        '2': { name: '2', email: '2' },
+        '3': { name: '3', email: '3' },
+        '4': { name: '4', email: '4' },
+      };
+      const testNewDB = {
+        '1': { name: '1', email: '2' },
+        '2': { name: '1', email: '2' },
+        '3': { name: '4', email: '4' },
+        '5': { name: '5', email: '5' },
+        '6': { name: '6', email: '6' },
+      };
+
+      const testReport = createReportObject(testOldDB, testNewDB);
+
+      const expectedReportObject = {
+        corrupt: {
+          '1': { corruptValues: { email: '2' }, actualValues: { email: '1' } },
+          '2': { corruptValues: { name: '1' }, actualValues: { name: '2' } },
+          '3': { corruptValues: { name: '4', email: '4' }, actualValues: { name: '3', email: '3' } },
+        },
+        missing: {
+          '4': { name: '4', email: '4' },
+        },
+        new: {
+          '5': { name: '5', email: '5' },
+          '6': { name: '6', email: '6' },
+        },
+      }
+      expect(testReport).toEqual(expectedReportObject);
+    });
+
+    it(`should be able to handle an extra field in the new DB`, () => {
+      const testOldDB = {
+        '1': { name: '1', email: '1' },
+        '2': { name: '2', email: '2' },
+        '3': { name: '3', email: '3' },
+        '4': { name: '4', email: '4' },
+      };
+      const testNewDB = {
+        '1': { name: '1', email: '2', extra: '1' },
+        '2': { name: '1', email: '2', extra: '2' },
+        '3': { name: '4', email: '4', extra: '3' },
+        '5': { name: '5', email: '5', extra: '4' },
+        '6': { name: '6', email: '6', extra: '5' },
+      };
+
+      const testReport = createReportObject(testOldDB, testNewDB);
+
+      const expectedReportObject = {
+        corrupt: {
+          '1': { corruptValues: { email: '2' }, actualValues: { email: '1' } },
+          '2': { corruptValues: { name: '1' }, actualValues: { name: '2' } },
+          '3': { corruptValues: { name: '4', email: '4' }, actualValues: { name: '3', email: '3' } },
+        },
+        missing: {
+          '4': { name: '4', email: '4' },
+        },
+        new: {
+          '5': { name: '5', email: '5', extra: '4' },
+          '6': { name: '6', email: '6', extra: '5' },
+        },
+      }
+      expect(testReport).toEqual(expectedReportObject);
     });
   });
 });
