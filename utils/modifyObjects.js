36588d8f38a95531707821a0f62cc92131f685db
Tandy Nguyen <nguyetan001@gmail.com>
2021-08-29 22:49:02 -0400

    Add comment for convertObjectToArrayOfObjects

--- utils/modifyObjects.js
+++ utils/modifyObjects.js
@@ -1,17 +1,17 @@
-/*** 
-  This function assumes the array (first argument) is an array of 
-  objects and each object has the same keys. You will have to specifiy
-  which key to use (second argument) amongst these objects
-  as the function will return a single object with each key
-  being the specified key's value, and each value for that key an object
-  containing the reamining keys.
-
-  const example = [{id: 1, name: 'Tom'}, {id: 2, name: 'Jerry'}]
-
-  const result = convertArrayToObjectWithKeys(example, 'id')
-
-  result === { '1': { name: 'Tom' }, '2': { name: 'Jerry' } }
-***/
+/**
+* This function assumes the array (first argument) is an array of 
+* objects and each object has the same keys. You will have to specifiy
+* which key to use (second argument) amongst these objects
+* as the function will return a single object with each key
+* being the specified key's value, and each value for that key an object
+* containing the reamining keys.
+*
+* const example = [{id: 1, name: 'Tom'}, {id: 2, name: 'Jerry'}]
+*
+* const result = convertArrayToObjectWithKeys(example, 'id')
+*
+* result === { '1': { name: 'Tom' }, '2': { name: 'Jerry' } }
+*/
 const convertArrayToObjectWithKeys = (arrayOfObjects, objectRowKey) => {
   let objectWithKeys = {};
 
@@ -25,6 +25,19 @@ const convertArrayToObjectWithKeys = (arrayOfObjects, objectRowKey) => {
   return objectWithKeys;
 }
 
+/**
+ * This function assumes the object (first argument) is an object
+ * with each key having a value of another object. The function will
+ * push each of these values into an array while additionally
+ * taking they key for each object and adding it as a key having the
+ * name of that key be specified by the second argment.
+ * 
+ * const example = { '1': { name: 'Tom' }, '2': { name: 'Jerry' } }
+ * 
+ * const result = convertObjectToArrayOfObjects(example, 'test')
+ * 
+ * result === [{test: 1, name: 'Tom'}, {test: 2, name: 'Jerry'}]
+ */
 const convertObjectToArrayOfObjects = (objectWithKeys, nameOfKey) => {
   let arrayOfObjects = [];
 
