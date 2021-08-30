eca2610efc693bf32ed461e2669a3c4b2355a443
Tandy Nguyen <nguyetan001@gmail.com>
2021-08-29 22:37:29 -0400

    Rename and add comment for convertArrayToObjectWithKeys

--- utils/modifyObjects.js
+++ utils/modifyObjects.js
@@ -1,9 +1,23 @@
-const convertArrayToObjectWithKeys = (array, colToKey) => {
+/*** 
+  This function assumes the array (first argument) is an array of 
+  objects and each object has the same keys. You will have to specifiy
+  which key to use (second argument) amongst these objects
+  as the function will return a single object with each key
+  being the specified key's value, and each value for that key an object
+  containing the reamining keys.
+
+  const example = [{id: 1, name: 'Tom'}, {id: 2, name: 'Jerry'}]
+
+  const result = convertArrayToObjectWithKeys(example, 'id')
+
+  result === { '1': { name: 'Tom' }, '2': { name: 'Jerry' } }
+***/
+const convertArrayToObjectWithKeys = (arrayOfObjects, objectRowKey) => {
   let objectWithKeys = {};
 
-  array.forEach((objectRow) => {
-    let id = objectRow[colToKey];
-    delete objectRow[colToKey];
+  arrayOfObjects.forEach((objectRow) => {
+    let id = objectRow[objectRowKey];
+    delete objectRow[objectRowKey];
 
     objectWithKeys[`${id}`] = { ...objectRow };
   });
